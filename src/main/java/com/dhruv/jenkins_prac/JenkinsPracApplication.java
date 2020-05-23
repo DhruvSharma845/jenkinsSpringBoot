3a94e62f9aeb87a2d137b5fbfea2432abb7aaacd
Dhruv Sharma <dhrsharm@Dhruvs-MacBook-Pro.local>
2020-05-23 14:00:13 +0530

    First Commit

---
+++ src/main/java/com/dhruv/jenkins_prac/JenkinsPracApplication.java
@@ -0,0 +1,13 @@
+package com.dhruv.jenkins_prac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsPracApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsPracApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dhruv/jenkins_prac/JenkinsPracApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dhruv.jenkins_prac;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsPracApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
